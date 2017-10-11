74efc5af214a56396431bc7e8f6883d4e9cd6414
klewiscse <bbslugger_99@yahoo.com>
2017-10-11 14:43:59 -0700

    Commented out json code w/room switcher
    Room switch works. Json Parser works. They don't work together. Too much SRAM we think is being used.

--- Arduino/AAILS.ino
+++ Arduino/AAILS.ino
@@ -3,7 +3,6 @@
 #include <ArduinoJson.h>
 
 System * sys;
-DynamicJsonBuffer jsonBuffer(200);
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,15 +10,30 @@ void setup() {
   System temp = System(5);
   sys = &temp;
   sys->getRoom(0)->set(6500,25);
+  /*char r [] = "Hello World";
+  StaticJsonBuffer<600> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(r);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+  } else {
+  const char* p = root["Cycles"][0]["ID"];
+  Serial.println(p);
+  }*/
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0){
-    String response = Serial.readString();
-    JsonObject &root = jsonBuffer.parseObject(response);
-    String p = root["cycles"];
-    Serial.println(p);
+    //Serial.println("Hello");
+    int n = Serial.parseInt();
+    Serial.println(n);
+    if (n == 0) sys->getRoom(n)->set(6500,25);
+    if (n == 1) sys->getRoom(n)->set(5000,50);
+    if (n == 2) sys->getRoom(n)->set(7000,25);
+    if (n == 3) sys->getRoom(n)->set(1500,50);
+    if (n == 4) sys->getRoom(n)->set(2500,50);
+    sys->switchTo(n);
   }
 
 }
