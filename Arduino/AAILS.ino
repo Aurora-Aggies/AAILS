2278427652faafa091be20494cb3c3be14790336
klewiscse <bbslugger_99@yahoo.com>
2017-10-11 14:57:17 -0700

    Added Json Parser into loop
    Room switch works. Json Parser works. They don't work together. Too much SRAM we think is being used.

--- Arduino/AAILS.ino
+++ Arduino/AAILS.ino
@@ -1,4 +1,3 @@
-#include "tempcolor.h"
 #include "aails.h"
 #include <ArduinoJson.h>
 
@@ -10,15 +9,6 @@ void setup() {
   System temp = System(5);
   sys = &temp;
   sys->getRoom(0)->set(6500,25);
-  /*char r [] = "Hello World";
-  StaticJsonBuffer<600> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(r);
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-  } else {
-  const char* p = root["Cycles"][0]["ID"];
-  Serial.println(p);
-  }*/
   
 }
 
@@ -33,6 +23,17 @@ void loop() {
     if (n == 2) sys->getRoom(n)->set(7000,25);
     if (n == 3) sys->getRoom(n)->set(1500,50);
     if (n == 4) sys->getRoom(n)->set(2500,50);
+    if (n == 5){
+      const char r [] PROGMEM = "{\"Cycles\":[{\"ID\":\"Sunset\",\"Temperature\":2700,\"Lumens\":1500,\"Percentage\":0.1},{\"ID\":\"Dawn\",\"Temperature\":3500,\"Lumens\":1700,\"Percentage\":0.1},{\"ID\":\"Overcast\",\"Temperature\":5000,\"Lumens\":2000,\"Percentage\":0.1},{\"ID\":\"Daylight\",\"Temperature\":6000,\"Lumens\":2200,\"Percentage\":0.1},{\"ID\":\"MidAfternoon\",\"Temperature\":4200,\"Lumens\":1900,\"Percentage\":0.1},{\"ID\":\"Dusk\",\"Temperature\":3500,\"Lumens\":1700,\"Percentage\":0.1},{\"ID\":\"Night\",\"Temperature\":2200,\"Lumens\":20,\"Percentage\":0.4}],\"HoursPerDay\":24}";
+      StaticJsonBuffer<600> jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(r);
+      if (!root.success()) {
+        Serial.println("parseObject() failed");
+      } else {
+      const char* p = root["Cycles"][0]["ID"];
+      Serial.println(p);
+      }
+    }
     sys->switchTo(n);
   }
 
