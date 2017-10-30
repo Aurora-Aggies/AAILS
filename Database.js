5f4fc3597b6612a213b46d6daee5944fbcc17a54
alyssav <alyssavaldez@tamu.edu>
2017-10-30 18:57:17 -0500

    added ejs files

---
+++ Database.js
@@ -0,0 +1,37 @@
+/***************************** Prerequisites *****************************/
+var Room = require("./Definitions.js");
+
+/***************************** Global Variables *****************************/
+var rooms = [];
+var phases = ["Sunset", "Dawn", "Overcast", "Daylight", "Mid-Afternoon", "Dusk", "Night"];
+
+/***************************** Creating Rooms *****************************/
+
+	// Room 1
+	var Temperatures1 = [2700, 3500, 5000, 6000, 4200, 3500, 2200];
+	var Lumens1 = [1500, 1700, 2000, 2200, 1900, 1700, 20];
+	var Percentages1 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4];
+
+
+	let room1 = new Room(1, Temperatures1, Lumens1, Percentages1);
+	rooms.push(room1);
+
+	// Room 2
+	var Temperatures2 = [2700, 2700, 2700, 2700, 2700, 2700, 2700];
+	var Lumens2 = [1700, 1700, 1700, 1700, 1700, 1700, 1700];
+	var Percentages2 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4];
+
+
+	let room2 = new Room(2, Temperatures2, Lumens2, Percentages2);
+	rooms.push(room2);
+
+/***************************** Http Requests *****************************/
+
+var myStr = JSON.stringify(room1);	// convert JSON to string
+console.log(myStr);
+
+
+
+/***************************** Exports *****************************/
+exports.rooms = rooms;
+exports.phases = phases;
--- Server.js
+++ Server.js
@@ -1,36 +1,40 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+
+// paths
+var database = require(__dirname + '/Database.js');
 var path = __dirname + '/views/';
 
-app.use("/assets", express.static(path + '/assets'));
+app.use(express.static(path));
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+// ejs
+//app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){ 
+ 	res.render(path + 'index', {name: "Adminstrator Dashboard"});
 });
+
+app.get('/room1', function(req, res){ 
+ 	res.render(path + 'room1', {name: "Room 1", room: database.rooms[0], phases: database.phases});
+});
+
+// body parser
+/*var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));	//for parsing of request body
 
 router.get("/",function(req,res){
   res.sendFile(path + "dashboard.html");
 });
 
 router.get("/room1",function(req,res){
-  res.sendFile(path + "room1.html");
+ res.sendFile(path + "room1.html");
+
 });
 
-/*router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
 
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});*/
-
-app.use("/",router);
-
-/*app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
-});*/
+app.use("/",router);*/
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
